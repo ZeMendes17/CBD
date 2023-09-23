2438d7c8b0846dbbb406a32d83baf7803002a9ba
Ze Mendes <zepedrosmendes@gmail.com>
2023-09-23 17:58:00 +0100

    1.5 from lab1 is done, only 1.6 lasts

--- Labs/Lab-1/ex5/src/main/java/redis/ex5/ServiceSystemB.java
+++ Labs/Lab-1/ex5/src/main/java/redis/ex5/ServiceSystemB.java
@@ -4,7 +4,9 @@ import redis.clients.jedis.Jedis;
 
 import java.util.Scanner;
 
-public class ServiceSystem {
+// in this exercise, the limit is quantity of products that can be ordered in a certain timeslot
+// instead of the number of orders that can be placed in a certain timeslot
+public class ServiceSystemB {
     private final Jedis jedis;
 
     // set with the users
@@ -14,21 +16,21 @@ public class ServiceSystem {
     public static String USER_TIMESLOT = "timeslot";
 
     // store how many products a user ordered in the current timeslot
-    public static String ORDER_COUNT = "order_count";
+    public static String PRODUCT_COUNT = "product_count";
 
     // store the orders for each user
-    public static String USER_ORDERS = "orders";
+    public static String USER_ORDERS = "orders"; // will store like --> orders:username = [product1:X, product2:Y, ...] (X and Y are the quantities)
 
     // error message displayed when the limit is about to be exceeded
     public static String ERROR_MESSAGE = "You cannot order more products. Please wait for the restriction to be over. " +
             "Time left: "; // will display the time that the user needs to wait
 
     // timeslot
-    public static int TIMESLOT = 2 * 60; // 2 minutes
+    public static int TIMESLOT = 5 * 60; // 5 minutes
     // limit of products that can be ordered
-    public static int LIMIT = 3;
+    public static int LIMIT = 10;
 
-    public ServiceSystem() {
+    public ServiceSystemB() {
         this.jedis = new Jedis("redis://localhost:6379");
     }
 
@@ -53,7 +55,7 @@ public class ServiceSystem {
         if (currentTime - initTime > TIMESLOT) {
             // the timeslot is over, reset the timeslot and the order count
             jedis.del(USER_TIMESLOT + ":" + username);
-            jedis.del(ORDER_COUNT + ":" + username);
+            jedis.del(PRODUCT_COUNT + ":" + username);
             return false;
         }
         return true;
@@ -64,7 +66,7 @@ public class ServiceSystem {
         // check if the user exceeded the limit
         if (checkTimeslot(username)) {
             // get the order count
-            int orderCount = Integer.parseInt(jedis.get(ORDER_COUNT + ":" + username));
+            int orderCount = Integer.parseInt(jedis.get(PRODUCT_COUNT + ":" + username));
             // check if the limit is exceeded
             if (orderCount >= LIMIT) {
                 // get the time left
@@ -75,20 +77,44 @@ public class ServiceSystem {
                 return;
             }
             // increment the order count otherwise
-            jedis.incr(ORDER_COUNT + ":" + username);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("How many would you like to order? ");
+            int quantity = sc.nextInt();
+            if (quantity + orderCount > LIMIT) { // check if the limit is exceeded with the new order
+                System.out.println("You cannot order more than " + LIMIT + " products!");
+                System.out.println("Try again.");
+                return;
+            }
+            jedis.incrBy(PRODUCT_COUNT + ":" + username, quantity);
+            // add the order to the user's orders
+            jedis.rpush(USER_ORDERS + ":" + username, product + ":" + quantity);
+            System.out.println("You ordered " + quantity + " - " + product);
         } else {
             // add the user to the timeslot set
             jedis.set(USER_TIMESLOT + ":" + username, (System.currentTimeMillis() / 1000) + "");
-            // set the order count to 1
-            jedis.set(ORDER_COUNT + ":" + username, "1");
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("How many would you like to order? ");
+            int quantity = sc.nextInt();
+            if (quantity > LIMIT) {
+                System.out.println("You cannot order more than " + LIMIT + " products!");
+                System.out.println("Try again.");
+                return;
+            }
+            if (quantity < 1) {
+                System.out.println("Invalid quantity!");
+                System.out.println("Try again.");
+                return;
+            }
+            jedis.set(PRODUCT_COUNT + ":" + username, quantity + "");
+            jedis.rpush(USER_ORDERS + ":" + username, product + ":" + quantity);
+            System.out.println("You ordered " + quantity + " - " + product);
         }
-        // add the order to the user's orders
-        jedis.rpush(USER_ORDERS + ":" + username, product);
     }
 
     public static void main(String[] args) {
         String[] products = {"sweatshirt", "t-shirt", "jeans", "jacket", "shoes", "socks", "scarf", "hat", "sunglasses"};
-        ServiceSystem serviceSystem = new ServiceSystem();
+        ServiceSystemB serviceSystem = new ServiceSystemB();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to our Service System!\n");
@@ -96,12 +122,12 @@ public class ServiceSystem {
         String username = sc.nextLine();
         serviceSystem.addUser(username);
 
-        System.out.println("You can make " + LIMIT + " orders in " + TIMESLOT + " seconds.\n");
+        System.out.println("You can order up to " + LIMIT + " products in " + TIMESLOT + " seconds.\n");
 
         int choice;
         while (true) {
             System.out.println("\nWhat would you like to do?\n");
-            System.out.println("1. Order a product");
+            System.out.println("1. Order products");
             System.out.println("2. Show available products");
             System.out.println("3. Show my orders");
             System.out.println("4. Check my timeslot");
@@ -112,6 +138,14 @@ public class ServiceSystem {
 
             switch (choice) {
                 case 1:
+                    if (serviceSystem.checkTimeslot(username) && Integer.parseInt(serviceSystem.jedis.get(PRODUCT_COUNT + ":" + username)) >= LIMIT) {
+                        // get the time left
+                        long currentTime = System.currentTimeMillis() / 1000;
+                        long initTime = Long.parseLong(serviceSystem.jedis.get(USER_TIMESLOT + ":" + username));
+                        long timeLeft = TIMESLOT - (currentTime - initTime);
+                        System.out.println(ERROR_MESSAGE + timeLeft + " seconds");
+                        break;
+                    }
                     System.out.println("Available products:");
                     for (int i = 0; i < products.length; i++)
                         System.out.println((i + 1) + ". " + products[i]);
@@ -129,8 +163,8 @@ public class ServiceSystem {
                         System.out.println("Invalid product number!");
                         break;
                     }
+
                     serviceSystem.orderProduct(username, products[Integer.parseInt(product) - 1]);
-                    System.out.println("You ordered " + products[Integer.parseInt(product) - 1]);
                     break;
                 case 2:
                     System.out.println("Available products:");
@@ -143,11 +177,12 @@ public class ServiceSystem {
                         break;
                     }
                     System.out.println("Your orders:");
+                    int i = 1;
                     for (String order : serviceSystem.jedis.lrange(USER_ORDERS + ":" + username, 0, -1))
-                        System.out.println(order);
+                        System.out.println(i++ + ". " + order.substring(0, order.indexOf(":")) + " - " + order.substring(order.indexOf(":") + 1));
                     break;
                 case 4:
-                    if (serviceSystem.checkTimeslot(username) && Integer.parseInt(serviceSystem.jedis.get(ORDER_COUNT + ":" + username)) >= LIMIT) {
+                    if (serviceSystem.checkTimeslot(username) && Integer.parseInt(serviceSystem.jedis.get(PRODUCT_COUNT + ":" + username)) >= LIMIT) {
                         // get the time left
                         long currentTime = System.currentTimeMillis() / 1000;
                         long initTime = Long.parseLong(serviceSystem.jedis.get(USER_TIMESLOT + ":" + username));
