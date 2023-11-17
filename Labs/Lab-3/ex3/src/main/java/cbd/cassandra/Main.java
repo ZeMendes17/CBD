a2f35e3264f454ec59250a67ca4a68aa9ce6559d
Zé Mendes <mendes.j@ua.pt>
2023-11-17 02:15:14 +0000

    Started 3.3 of lab3

---
+++ Labs/Lab-3/ex3/src/main/java/cbd/cassandra/Main.java
@@ -0,0 +1,30 @@
+package cbd.cassandra;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build()) {
+            Session session = cluster.connect("videos");
+
+            insertUser(session, "Ze", "ze@example.com", "José");
+            System.out.println("User inserted");
+            deleteUser(session, "Ze");
+            System.out.println("User deleted");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void insertUser(Session session, String username, String email, String name) {
+        session.execute("INSERT INTO user (username, email, name, registTime) VALUES ('" + username + "', '" + email + "', '" + name + "', toTimestamp(now()));");
+
+    }
+
+    private static void deleteUser(Session session, String username) {
+        session.execute("DELETE FROM user WHERE username = '" + username + "';");
+    }
+
+}
