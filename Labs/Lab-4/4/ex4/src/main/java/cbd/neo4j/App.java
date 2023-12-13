88ccf37559a37819e6662f8ffa74eadaa6fd5fb5
Zé Mendes <mendes.j@ua.pt>
2023-12-13 18:09:57 +0000

    done lab4 ex4 queries

--- Labs/Lab-4/4/ex4/src/main/java/cbd/neo4j/App.java
+++ Labs/Lab-4/4/ex4/src/main/java/cbd/neo4j/App.java
@@ -185,6 +185,100 @@ public class App {
         }
         out.write("\n\n");
 
+        // 6. A média de dinheiro gerado por cada produto ao longo de todas as encomendas, ordenado por ordem decrescente da média.
+        query = "MATCH (o:Order)-[:PURCHASED]->(p:Product)\n";
+        query += "WITH p.productCode AS code, AVG(o.sales) AS avgSales\n";
+        query += "RETURN code, avgSales\n";
+        query += "ORDER BY avgSales DESC";
+        out.write("6. A média de dinheiro gerado por cada produto ao longo de todas as encomendas, ordenado por ordem decrescente da média.\n");
+        out.write(query + "\n\n");
+        result = session.run(query);
+        out.write("Código do produto ---> Média de dinheiro gerado\n");
+        out.write("------------------------------------------------\n");
+        while (result.hasNext()) {
+            Record record = result.next();
+            String code = record.get("code").asString();
+            String avgSales = record.get("avgSales").toString();
+            out.write(code + " ---> " + avgSales + "\n");
+        }
+        out.write("\n\n");
+
+        // 7. Top 5 dos produtos que foram mais vezes comprados, com o nome do produto e o número de vezes que foi comprado.
+        query = "MATCH (o:Order)-[:PURCHASED]->(p:Product)\n";
+        query += "WITH p, COUNT(o) AS numOrders\n";
+        query += "RETURN p.productCode AS productCode, p.productLine AS productLine, numOrders AS NumOrders\n";
+        query += "ORDER BY numOrders DESC\n";
+        query += "LIMIT 5";
+        out.write("7. Top 5 dos produtos que foram mais vezes comprados, com o nome do produto e o número de vezes que foi comprado.\n");
+        out.write(query + "\n\n");
+        result = session.run(query);
+        out.write("Código do produto ---> Linha do produto; Número de vezes que foi comprado\n");
+        out.write("-------------------------------------------------------------------------\n");
+        while (result.hasNext()) {
+            Record record = result.next();
+            String productCode = record.get("productCode").asString();
+            String productLine = record.get("productLine").asString();
+            String numOrders = record.get("NumOrders").toString();
+            out.write(productCode + " ---> " + productLine + "; " + numOrders + "\n");
+        }
+        out.write("\n\n");
+
+        // 8. Listar o MSRP (Manufacturer's Suggested Retail Price) por linha de produto.
+        query = "MATCH (p:Product)\n";
+        query += "RETURN p.productLine AS productLine, AVG(p.msrp) AS msrp\n";
+        out.write("8. Listar o MSRP (Manufacturer's Suggested Retail Price) por linha de produto.\n");
+        out.write(query + "\n\n");
+        result = session.run(query);
+        out.write("Linha do produto ---> MSRP\n");
+        out.write("---------------------------\n");
+        while (result.hasNext()) {
+            Record record = result.next();
+            String productLine = record.get("productLine").asString();
+            String msrp = record.get("msrp").toString();
+            out.write(productLine + " ---> " + msrp + "\n");
+        }
+        out.write("\n\n");
+
+        // 9. Listar os 10 produtos mais vendidos de em uma única encomenda.
+        query = "MATCH (p:Product)<-[r:PURCHASED]-(:Order)\n";
+        query += "WITH p, MAX(r.quantityOrdered) AS maxQuantityOrdered\n";
+        query += "RETURN p.productCode, maxQuantityOrdered\n";
+        query += "ORDER BY maxQuantityOrdered DESC\n";
+        query += "LIMIT 10";
+        out.write("9. Listar os 10 produtos mais vendidos de em uma única encomenda.\n");
+        out.write(query + "\n\n");
+        result = session.run(query);
+        out.write("Código do produto ---> Número de produtos vendidos\n");
+        out.write("---------------------------------------------------\n");
+        while (result.hasNext()) {
+            Record record = result.next();
+            String productCode = record.get("p.productCode").asString();
+            String maxQuantityOrdered = record.get("maxQuantityOrdered").toString();
+            out.write(productCode + " ---> " + maxQuantityOrdered + "\n");
+        }
+        out.write("\n\n");
+
+        // 10. Listar o número de produtos vendidos (código e linha do produto) a cada país, ordenado por ordem decrescente do número de produtos vendidos.
+        query = "MATCH (co:Country)<-[:LOCATED_IN_COUNTRY]-(:Customer)-[:PLACED_ORDER]->(:Order)-[r:PURCHASED]->(p:Product)\n";
+        query += "WITH co, p, SUM(r.quantityOrdered) AS totalQuantityOrdered\n";
+        query += "ORDER BY co.country, totalQuantityOrdered DESC\n";
+        query += "WITH co, COLLECT({product: p, totalQuantityOrdered: totalQuantityOrdered})[0] AS topProductPerCountry\n";
+        query += "RETURN co.country AS Country, topProductPerCountry.product.productCode AS ProductCode, topProductPerCountry.product.productLine AS ProductLine, topProductPerCountry.totalQuantityOrdered AS TotalQuantityOrdered\n";
+        query += "ORDER BY TotalQuantityOrdered DESC";
+        out.write("10. Listar o número de produtos vendidos (código e linha do produto) a cada país, ordenado por ordem decrescente do número de produtos vendidos.\n");
+        out.write(query + "\n\n");
+        result = session.run(query);
+        out.write("País ---> Código do produto; Linha do produto; Número de produtos vendidos\n");
+        out.write("--------------------------------------------------------------------------\n");
+        while (result.hasNext()) {
+            Record record = result.next();
+            String country = record.get("Country").asString();
+            String productCode = record.get("ProductCode").asString();
+            String productLine = record.get("ProductLine").asString();
+            String totalQuantityOrdered = record.get("TotalQuantityOrdered").toString();
+            out.write(country + " ---> " + productCode + "; " + productLine + "; " + totalQuantityOrdered + "\n");
+        }
+
         driver.close();
         out.close();
     }
